f0d51385e70a327c9b1cbc49507b6c090b5ca84d
Robert Shilcof <robert.shilcof@gmail.com>
2020-10-08 10:54:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+const lis = document;
 
+
+function getFirstSelector(selector) {
+  var selection = lis.querySelector(selector);
+  return selection;
+}
+
+function nestedTarget() {
+  var selection = lis.querySelector('#nested .target');
+  return selection
+}
+
+function increaseRankBy(n) {
+  var selection = lis.querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < selection.length; i++) {
+    selection[i].innerHTML = (parseInt(selection[i].innerHTML, 10) + n).toString();
+  }
+  return selection;
+}
+
+function deepestChild() {
+  var selection = lis.querySelectorAll('#grand-node div');
+  return selection[selection.length-1];
+}
