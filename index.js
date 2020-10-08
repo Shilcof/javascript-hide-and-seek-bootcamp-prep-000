516eccdda341237858c616cc65f26dc57c1bcd8a
Robert Shilcof <robert.shilcof@gmail.com>
2020-10-08 10:25:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const lis = document;
 
+
+function getFirstSelector(selector) {
+  var selection = lis.querySelector(selector);
+  return selection;
+}
+
+function nestedTarget() {
+  
+}
+
+function increaseRankBy(n) {
+  
+}
+
+function deepestChild() {
+  
+}
